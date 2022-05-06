eebb1373645e74c7ba68f6ad09eaf25d4b1bd3bc
Suraphel Mekasha <zewdusu@hotmail.com>
2022-05-06 11:08:54 +0200

    fromThinkpad

---
+++ Data/Model/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace fullVersion.Data.Model
+{
+    public class Book
+    {
+        public int Id  { get; set; }
+
+        public string  Title { get; set; }
+
+        public string Author { get; set; }
+
+        public DateTime AddDate { get; set;}
+
+        public bool IsRead { get; set; }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,68 @@
+using fullVersion.Data.Model;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace fullVersion
+{
+    public class Startup
+    {
+
+        public string connectionString { get; set; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+            connectionString = Configuration.GetConnectionString("DefaultConnectionString"); 
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+
+            // Configuer the DbContext with the SQL
+            services.AddDbContext<BookContext>(options => options.UseSqlServer(connectionString));
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "fullVersion", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "fullVersion v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
